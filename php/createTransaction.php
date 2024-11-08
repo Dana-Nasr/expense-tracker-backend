402ca389115d376621dfddd4b8ba6bce169e7eb1
dananasr2004 <dananasr871@gmail.com>
2024-11-08 17:30:47 +0200

    Edit: createTransaction.php

--- php/createTransaction.php
+++ php/createTransaction.php
@@ -2,12 +2,12 @@
 include "./connection.php";
 $data = json_decode(file_get_contents("php://input"), true);
     $user_id = 1;  
-    $amount = $_POST["amount"];    
-    $description = $_POST['notes']; 
-    $date =$_POST['date'];
-    $type= $_POST['type'];
+    $amount =  $_POST["amount"] ?? null;    
+    $notes = $_POST["notes"]?? null; 
+    $date =$_POST['date']?? null;
+    $type= $_POST['type']?? null;
     
-    $query = $connection->prepare("INSERT INTO transactions (users_user_id, 'type', amount, notes, 'date') VALUES ( $user_id ,$type, $amount , $description,$date )");
+    $query = $connection->prepare("INSERT INTO transactions (users_user_id, `type`, amount, notes, `date`) VALUES ( $user_id ,$type, $amount , $notes,$date )");
     if ($query->execute()) {
       
         echo json_encode(["message" => "Transaction created successfully!"]);
